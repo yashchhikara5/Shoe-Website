9e33d6b8a29363b7313d1fda51f7756364c882af
Priya-Manchanda <priyamanchanda1108@gmail.com>
2022-08-27 11:47:34 +0530

    First

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+var userIsRegistered = false;
 function Account() {
   return (
-    <div>
-      <h2>Log In</h2>
-      <div>
-        <input type="email" placeholder="E-Mail"></input>
-      </div>
-      <div>
-        <input type="password" placeholder="Password"></input>
-      </div>
-      <input type="submit" value="LOG IN"></input>
-      Already have an account?
-      <input type="submit" value="Sign Up"></input>
+    <div className="account">
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        {!userIsRegistered && (
+          <input type="password" placeholder="Confirm Password" />
+        )}
+        <div className="button_align">
+          <button type="submit">
+            {userIsRegistered ? "Login" : "Register"}
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
