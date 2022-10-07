0dc4ccef6c283c1ddf64daef4283efadc1018f32
Priya Manchada <priyamanchanda1108@gmail.com>
2022-10-07 10:20:02 +0530

    First

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,24 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import Login from "./Login";
-function Account() {
+function Login() {
   const [data, setData] = useState({
     username: "",
-    email: "",
     Password: "",
   });
   const handleinput = ({ currentTarget: input }) => {
     setData({ ...data, [input.name]: input.value });
   };
   const [error, setError] = useState("");
-  const Navigate = useNavigate();
   const handleChange = async (event) => {
     event.preventDefault();
     try {
-      const url = "http://localhost:7000/api/user/Register";
+      const url = "http://localhost:7000/api/user/login";
       const { data: res } = await axios.post(url, data);
-      Navigate("/Login");
       console.log(res.message);
     } catch (error) {
       if (
@@ -31,11 +26,11 @@ function Account() {
     }
   };
   function handleLogin() {
-    console.log("login");
+    console.log("Register");
   }
   return (
     <div>
-      <h1 className="text-center Login">Register Here</h1>
+      <h1 className="text-center Login">Login Here</h1>
       <div className="account">
         <form className="form" onSubmit={handleChange}>
           <input
@@ -46,27 +41,20 @@ function Account() {
             onChange={handleinput}
           />
           <input
-            type="email"
-            placeholder="Email"
-            value={data.email}
-            name="email"
-            onChange={handleinput}
-          />
-          <input
             type="password"
             placeholder="Password"
             value={data.Password}
             name="Password"
             onChange={handleinput}
           />
-          {error && <div>Error</div>}
+          {error && <div>{error}</div>}
           <div className="button_align">
             <button className="Register" type="submit">
-              Register
+              Login
             </button>
           </div>
           <div className="login">
-            Already have an account?<p onClick={handleLogin}> Login</p>
+            Don't have an account?<p onClick={handleLogin}> Register</p>
           </div>
         </form>
       </div>
@@ -74,4 +62,4 @@ function Account() {
   );
 }
 
-export default Account;
+export default Login;
